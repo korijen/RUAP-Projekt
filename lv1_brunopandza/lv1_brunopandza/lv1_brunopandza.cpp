beeae55418297052c32c09cdfcb93c8082eb8dd9
Bruno Pandza <bruno.pandza99@gmail.com>
2021-11-03 19:51:53 +0100

    konflikt rijesen

--- lv1_brunopandza/lv1_brunopandza/lv1_brunopandza.cpp
+++ lv1_brunopandza/lv1_brunopandza/lv1_brunopandza.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
+<<<<<<< HEAD
     std::cout << "Pozdrav iz aplikacije od Bruno Pandza, index : 10\n";
+=======
+    std::cout << "Pozdrav iz aplikacije od Bruno Pandza\n";
+>>>>>>> 679c2be (Dodana linija za ispis)
 }
 
